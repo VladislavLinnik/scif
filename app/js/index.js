8f73d6dfbb601b94090324c42ba1739619a08e63
Vladislav <vlad.l@houstonapps.co>
2019-08-06 16:30:20 +0300

    updated

--- app/js/index.js
+++ app/js/index.js
@@ -16,10 +16,10 @@ function stickyHeader() {
 
 
 // registration form
-// $('.js-firstStep').hide();
+$('.js-firstStep').hide();
 $('.js-secondStep').hide();
 $('.js-thirdStep').hide();
-$('.js-fourStep').hide();
+$('.js-fourStep').show();
 $('.js-finalStep').hide();
 
 $('.js-registerCircle').eq(0).addClass('active');
@@ -117,58 +117,6 @@ $('#checkboxInput4').on('change', function() {
         $('#otherOption2').hide();
     }
 });
-
-
-
-// $('.js-registerFormSelect').each(function(){
-//     var $this = $(this), numberOfOptions = $(this).children('option').length;
-  
-//     $this.addClass('select-hidden'); 
-//     $this.wrap('<div class="select"></div>');
-//     $this.after('<div class="select-styled"></div>');
-
-//     var $styledSelect = $this.next('div.select-styled');
-//     $styledSelect.text($this.children('option').eq(0).text());
-  
-//     var $list = $('<ul />', {
-//         'class': 'select-options'
-//     }).insertAfter($styledSelect);
-  
-//     for (var i = 0; i < numberOfOptions; i++) {
-//         $('<li />', {
-//             text: $this.children('option').eq(i).text(),
-//             rel: $this.children('option').eq(i).val()
-//         }).appendTo($list);
-//     }
-  
-//     var $listItems = $list.children('li');
-  
-//     $styledSelect.click(function(e) {
-//         e.stopPropagation();
-//         $('div.select-styled.active').not(this).each(function(){
-//             $(this).removeClass('active').next('ul.select-options').hide();
-//         });
-//         $(this).toggleClass('active').next('ul.select-options').toggle();
-//     });
-  
-//     $listItems.click(function(e) {
-//         e.stopPropagation();
-//         $styledSelect.text($(this).text()).removeClass('active');
-//         $this.val($(this).attr('rel'));
-//         $list.hide();
-//         //console.log($this.val());
-//     });
-  
-//     $(document).click(function() {
-//         $styledSelect.removeClass('active');
-//         $list.hide();
-//     });
-
-// });
-
-
-
-
 
 
 
